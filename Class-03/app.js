0d47902166997d9c8ac3cd17e197fe2e55db4ab0
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-07-06 21:06:28 +0500

    Class 03 code updated

---
+++ Class-03/app.js
@@ -0,0 +1,233 @@
+// var num = 10;
+// console.log(num);
+
+/*
+var num1 = 10;
+var num2 = 4;
+var addition = num1 + num2;
+var subtraction = num1 - num2;
+var multiply = num1 * num2;
+var divide = num1 / num2;
+var reminder = 10 % 3;
+
+console.log(reminder);
+*/
+
+/*
+var firstName = "muhammad";
+var lastName = "ahmed";
+var fullname = firstName + lastName;
+console.log("Hi " + fullname);
+*/
+
+// var equation = 761 * 100 / 850;
+// console.log(equation + " %");
+
+// var num = 10;
+// console.log(num + 30);
+
+// var euqation = (10 + 5) * 5;
+// console.log(euqation);
+
+// var userName = prompt("What is your name?");
+// console.log(userName);
+
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// Note: If Statements...!
+/*
+var name = "muhammad";
+var userName = prompt('What is your name?');
+
+// == only check values.
+// === check value and it's data type.
+
+if ( userName == "ahmed" ) {
+    console.log('Name is valid');
+}
+
+else if (userName == name) {
+    console.log(true);
+}
+
+else {
+    console.log("Name is invalid");
+}
+*/
+
+// var myName = "ahmed";
+// var userName = prompt('Enter Your Name');
+
+// if ((userName == myName) || (userName == "muhammad")) {
+//     console.log(true);
+// }
+
+// else {
+//     console.log(false);
+// };
+
+// var randomNum = 8
+// var userInput = prompt('Enter any number');
+
+// if (userInput >= 5) {
+//     console.log(userInput + " is grater than 5");
+// }
+
+// else {
+//     console.log(userInput + " is not grater than 5");
+// }
+
+// if (userInput != randomNum) {
+//     console.log("Value did not match!");
+// }
+
+// else {
+//     console.log('Value matched!');
+// }
+
+// // var userName = prompt('Enter your name?');
+// // document.write("Hi ", userName);
+
+// var userName = prompt('Enter your name?');
+// var myName = "ahmed";
+// var firstName = "muhammad";
+// var nameFound = false;
+
+// if (userName == myName) {
+//     nameFound = true;
+
+//     if (nameFound == true) {
+//         console.log('Valid Name');
+//     };
+// }
+
+// else {
+//     console.log("Invalid Name");
+// }
+
+// var name = 'ahmed';
+// console.log(name);
+// console.log(typeof name);
+
+// var num = 10;
+// console.log(num);
+// console.log(typeof num);
+
+// var firstName = "muhammad";
+// var lastName = "ahmed";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// var firstNum = 10;
+// var secondNum = 3;
+
+// var addition = firstNum + secondNum;
+// var subtraction = firstNum - secondNum;
+// var multiply = firstNum * secondNum;
+// var divide = firstNum / secondNum;
+// var modulous = firstNum % secondNum;
+// console.log(modulous);
+
+// var _myFirstName = "muhammad";
+// console.log(_myFirstName);
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// console.log(popularNumber)
+
+// var equation = 761 * 100 / 850;
+// console.log(equation + " %");
+
+// var userName = prompt("Enter your name!");
+// console.log(userName);
+
+// var firstName = "muhammad";
+// var lastName = "ahmed";
+// var fullName = firstName + "\n" + lastName;
+// console.log(fullName);
+
+// var firstName = prompt("Enter your first name?")
+// var lastName = prompt("Enter your last name?")
+// var fullName = firstName + "\n" + lastName;
+// console.log(fullName);
+
+// var question = "Enter any number";
+// var askUser = prompt(question);
+// console.log(askUser);
+// console.log(typeof askUser);
+
+// var myName = "ahmed";
+// console.log(myName);
+// console.log(typeof myName);
+
+// Note: == only check values.
+// Note: === check values + data type.
+
+// if (myName == "ahmed") {
+//     console.log(true);
+// }
+
+// else {
+//     console.log(false);
+// };
+
+// var userName = prompt('Enter your name');
+
+// if (userName == "muhammad") {
+//     console.log(userName + " is your first name");
+// }
+
+// else if (userName == "ahmed") {
+//     console.log(userName + " is your last name");
+// }
+
+// else {
+//     console.log("Inavlid Name!");
+// }
+
+// var num = "8";
+// num = Number(num);
+// console.log(num);
+// console.log(typeof num);
+
+// var userInput = prompt('Enter any number!');
+
+// if (userInput >= 5) {
+//     console.log(userInput + " is grater than 5");
+// }
+
+// else if (userInput < 3) {
+//     console.log(userInput + " is less than 3");
+// }
+
+// else {
+//     console.log("Invalid");
+// }
+
+// var userInput = Number(prompt('Enter any number!'));
+// console.log(userInput);
+// console.log(typeof userInput);
+
+// if ((userInput > 0) && (typeof userInput == "number")) {
+//     console.log(userInput);
+// }
+
+// else {
+//     console.log(false);
+// }
+
+var userInput = prompt('Enter any number!');
+// console.log(userInput);
+// console.log(typeof userInput);
+
+if ((userInput == "muhammad") || (userInput == "ahmed")) {
+    console.log(userInput);
+}
+
+else {
+    console.log("Invalid Name!");
+}

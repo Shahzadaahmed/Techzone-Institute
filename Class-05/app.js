007e1e7ae7115d5c4df225511fda44c4d1d65aa0
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-07-18 21:09:53 +0500

    Learning DOM

--- Class-05/app.js
+++ Class-05/app.js
@@ -60,17 +60,57 @@ else if ((userInput > 0) || (userInput < 0)) {
 */
 
 // Example # 06:
-var userInput = prompt("Enter any number or character!");
-// console.log(userInput);
+// var userInput = prompt("Enter any number or character!");
+// // console.log(userInput);
 
-if (userInput >= "A" && userInput <= "Z") {
-    console.log(userInput, " is an uppercase character!");
-}
+// if (userInput >= "A" && userInput <= "Z") {
+//     console.log(userInput, " is an uppercase character!");
+// }
 
-else if (userInput >= "a" && userInput <= "z") {
-    console.log(userInput, " is an lowercase character!");
-}
+// else if (userInput >= "a" && userInput <= "z") {
+//     console.log(userInput, " is an lowercase character!");
+// }
 
-else if ((userInput > 0) || (userInput < 0)) {
-    console.log(userInput, " is a number!");
-};
+// else if ((userInput > 0) || (userInput < 0)) {
+//     console.log(userInput, " is a number!");
+// };
+
+// var useInput = prompt("Enter any number!");
+// // console.log(useInput);
+
+// if (useInput % 3 == 0) {
+//     console.log(useInput + " is divisible by 3!");
+// }
+
+// else {
+//     console.log(useInput + " is not divisible by 3!");
+// }
+
+// var num1 = Number(prompt("Enter any number!"));
+// var num2 = Number(prompt("Enter any another number!"));
+// var operator = prompt("Enter operator");
+// var result;
+
+// if (operator == "+") {
+//     result = num1 + num2;
+//     console.log(result);
+// }
+
+// else {
+//     console.log(false);
+// }
+
+// var palindrome = prompt("Enter any word to check it is palindrome or not!");
+// var palindromeFormula = palindrome.split("").reverse().join("");
+
+// if (palindrome == palindromeFormula) {
+//     console.log("This is palindrome!");
+// }
+
+// else {
+//     console.log("This is not palindrome!");
+// }
+
+var userName = prompt("Enter your name");
+var heading = document.getElementById("heading");
+heading.innerHTML = userName;

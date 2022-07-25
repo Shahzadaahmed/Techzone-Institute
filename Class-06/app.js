51be72479bbe0e6bafc5ba4bb71c5aa454b6ef10
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-07-25 16:14:17 +0500

    latest code updated

---
+++ Class-06/app.js
@@ -0,0 +1,49 @@
+// alert("Hi, Ahmed!");
+// alert('Hi AHmed');
+
+// console.log("Hello World");
+// console.log('Hi AHmed');
+
+// var myPassword = "ahmed1996";
+// var userInput = prompt("Enter your password!");
+// // console.log(userInput);
+
+// if (userInput == "") {
+//     console.log("Please enter password!");
+// }
+
+// else if (userInput != myPassword) {
+//     console.log("Password does not match with the origional password");
+// }
+
+// else if (userInput == myPassword) {
+//     console.log("Password macthed! 😄");
+// }
+
+// var userInput = document.getElementById("user-input");
+// console.log(userInput.value);
+
+// Note: Learning functions...!
+// function greetUser() {
+//     console.log("Helo Ahmed");
+// }
+
+// greetUser();
+
+// greetUser();
+
+// greetUser();
+
+// function askUserName(userName) {
+//     console.log("Hi ", userName);
+// }
+
+// var myName = prompt("Enter your name!");
+// askUserName("ahmed");
+// askUserName("ali");
+// askUserName("umer");
+// askUserName("akbar");
+// askUserName("asghar");
+
+// alert("Hi\nAhmed ");
+// console.error("Error");
---
+++ Class-07/app.js
@@ -0,0 +1,99 @@
+// var myUserName = "ahmed";
+// console.log(name);
+
+// var &name = "ahmed";
+// // console.log(-name);
+
+// var firstName = "muhammad";
+// var lastName = "ahmed";
+// var fullName = firstName + " " + lastName;
+// console.log("Hi " + fullName);
+
+// var userInput = prompt("Enter your name!");
+// // console.log(userInput);
+
+// if (userInput == "ahmed") {
+//     console.log("Good");
+// }
+
+// else if (userInput == "muhammad") {
+//     console.log("It's Ok!");
+// }
+
+// else {
+//     console.log("Bad");
+// }
+
+// var userInput = prompt("Enter your age!");
+
+// if (userInput > 18) {
+//     console.log('Adult')
+// }
+
+// else {
+//     console.log("Children");
+// }
+
+// if (userInput >= 1 && userInput <= 10) {
+//     console.log("Matched");
+// }
+
+// else {
+//     console.log('Invalid');
+// }
+
+// var names = ["ahmed", "umer", "akbar", "asghar", "bilal", 1996, true, "ali", undefined, 08, NaN];
+// console.log(names);
+// console.log(names.length);
+
+// var itemOne = names[19];
+// console.log(itemOne);
+
+// names.push("yaseen"); // add in last.
+// console.log(names);
+
+// names.unshift("asif") // add in start.
+// console.log(names);
+
+// names.pop(); // removes from last
+// console.log(names);
+
+// names.shift(); // removes from start
+// console.log(names);
+
+// var modifyArr = names.slice(1, 6);
+// console.log(modifyArr);
+
+// names.splice(1, 3);
+// console.log(names);
+
+// names.splice(0, 1, "asad");
+// console.log(names);
+
+// names.slice(0, 10);
+// console.log(names);
+
+var userInput_1 = prompt("Enter name one");
+var userInput_2 = prompt("Enter name two");
+var userInput_3 = prompt("Enter name three");
+var userInput_4 = prompt("Enter name four");
+
+// Solution: 1
+// var arr = [userInput_1, userInput_2, userInput_3, userInput_4];
+// console.log(arr);
+
+// Solution: 2
+// var arr = [];
+// arr.push(userInput_1);
+// arr.push(userInput_2);
+// arr.push(userInput_3);
+// arr.push(userInput_4);
+// console.log(arr);
+
+// Solutio: 3
+// var arr = [];
+// arr[0] = userInput_1;
+// arr[1] = userInput_2;
+// arr[2] = userInput_3;
+// arr[3] = userInput_4;
+// console.log(arr);

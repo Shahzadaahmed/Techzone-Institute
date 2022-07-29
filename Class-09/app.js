702900e70957ee300f34a5dee9e32b90eacd4e80
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-07-29 19:23:40 +0500

    Class 8 code updated

---
+++ Class-09/app.js
@@ -0,0 +1,25 @@
+var namesArr = ["ahmed", "mehran", "shahzaib", "kashan", "khurram", "shahzaib"];
+// var elementIndex = namesArr.indexOf("abc");
+// console.log(elementIndex);
+
+// var el = namesArr.charAt(0);
+// console.log(el);
+
+// var arr = [
+//     [1, 2, 3],
+//     [3, 4, 5],
+//     [6, 7, 8]
+// ];
+// console.log(arr[0][2]);
+
+// document.write(namesArr.indexOf(namesArr[0]), " ", namesArr[0], "<br />");
+// document.write(namesArr.indexOf(namesArr[1]), " ", namesArr[1], "<br />");
+// document.write(namesArr.indexOf(namesArr[2]), " ", namesArr[2], "<br />");
+// document.write(namesArr.indexOf(namesArr[3]), " ", namesArr[3], "<br />");
+// document.write(namesArr.indexOf(namesArr[4]), " ", namesArr[4], "<br />");
+// document.write(namesArr.indexOf(namesArr[5]), " ", namesArr[5], "<br />")
+
+// var startingIndex = prompt("At which index do you want to delete name?");
+// var numsOfData = prompt("How many names do you want to delete?");
+// namesArr.splice(startingIndex, numsOfData);
+// console.log(namesArr);

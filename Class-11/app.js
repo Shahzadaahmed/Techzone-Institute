42030a2c2ccfe9d897e372412c8a8afbbe226cb6
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-08-03 20:38:50 +0500

    COde updated

---
+++ Class-11/app.js
@@ -0,0 +1,62 @@
+// var myName = "ahmed";
+// var symbolFound = false;
+
+// for (var i = 0; i < myName.length; i++) {
+//     // console.log(myName[i]);
+
+//     if (myName.charAt(i) == "!") {
+//         symbolFound = true;
+//         break;
+//     };
+// };
+
+// if (symbolFound) {
+//     console.log("Symbod found!");
+// }
+
+// else {
+//     console.log("Symbod not found!");
+// };
+
+// var myName = "ahmed";
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (var i = 10; i >= 5; i--) {
+//     console.log(i);
+// }
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+
+//     if (i == "6") {
+//         break;
+//     };
+// };
+
+// var arr = ["ahmed", "mehran", "kashan", "shahzaib", "khurram"];
+var myName = "ahm!ed";
+var symbolFound = false;
+
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(i, arr[i]);
+// }
+
+for (var i = 0; i < myName.length; i++) {
+    console.log(i, myName[i]);
+
+    if (myName.charAt(i) == "!") {
+        symbolFound = true;
+        break;
+    };
+};
+
+if (symbolFound) {
+    console.log('Symbol found!');
+}
+
+else {
+    console.log('Symbol not found!');
+}

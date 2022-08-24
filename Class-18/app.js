230bde64542f0b5ef85ae310d9a4abf1690c1842
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-08-24 20:22:56 +0500

    Books updated

---
+++ Class-18/app.js
@@ -0,0 +1,60 @@
+// var myName = 'ahmed';
+// myName = "muhamad ahmed";
+
+// var num = "1996";
+// var anotherNum = 8;
+
+// console.log(num);
+// console.log(typeof num);
+// console.log(anotherNum);
+// console.log(typeof anotherNum);
+
+// var firstNum = 6;
+// var secondNum = 4;
+// var addition = firstNum + secondNum;
+// var subtraction = firstNum - secondNum;
+// console.log(subtraction);
+
+// var _myName = "ahmed";
+// console.log(_myName);
+
+// var addValues = 2 + 2;
+// console.log(addValues);
+
+// var myPersentage = 771 * 100 / 850;
+// console.log(myPersentage);
+
+// var remindVal = 10 % 3;
+// console.log(remindVal);
+
+// var num = 2;
+// num = num + 1;
+// console.log(++num);
+
+// var num = 3;
+// console.log(num++);
+
+// for (i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// var num = 6;
+// console.log(--num);
+
+// var num = 7;
+// console.log(num--);
+
+// for (var i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// var cond = (2 + 2) * 3;
+// console.log(cond);
+
+// var firstName = "shahzada";
+// var lastName = "muhammad";
+// var fullName = firstName + " " + lastName + " ahmed " + "rana";
+// console.log(fullName);
+
+// var userInput = prompt("Enter your name!", "ahmed");
+// console.log(userInput);

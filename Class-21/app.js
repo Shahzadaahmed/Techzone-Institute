fd2b39e6cc8f893c1bca6bf0a4d672e157498eb6
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-08-31 20:23:50 +0500

    COde updated

---
+++ Class-21/app.js
@@ -0,0 +1,109 @@
+// function greetUser(name) {
+//     console.log("Hi", name);
+// };
+
+// greetUser("ahmed");
+// greetUser("asif");
+// greetUser("mohsin");
+// greetUser();
+
+// function addition(num1, num2) {
+//     // console.log(num1, num2);
+
+//     var addData = num1 + num2;
+//     console.log(addData);
+// };
+
+// addition(2, 4);
+
+
+
+
+
+// function helloUser() {
+//     var greeting = "Welsome to JS World!";
+//     return greeting;
+// };
+
+// var result = helloUser();
+// console.log(result);
+
+
+
+
+
+
+
+
+// function subtraction(num1, num2) {
+//     return num1 - num2;
+// }
+
+// var result = subtraction(10, 6);
+// console.log(result);
+
+
+
+// function testingCondition(userName) {
+//     if (userName == "ahmed") {
+//         return "Class on!";
+//     }
+
+//     else {
+//         return "Class off!";
+//     }
+// }
+
+// var result = testingCondition("mahmed");
+// console.log(result);
+
+
+
+
+// function userInfo(ahmedata) {
+//     console.log(ahmedata);
+// }
+
+// let myData = {
+//     myName: "ahmed",
+//     title: "Software Engr"
+// };
+
+// userInfo(myData);
+
+// function greetUser() {
+//     return "Hello";
+// };
+
+// function callName(name, handler) {
+//     console.log(handler(), name);
+// };
+
+// callName("asif", greetUser);
+
+
+
+
+
+
+
+
+
+
+// function testing(data) {
+//     console.log(data);
+// }
+
+// var myData = {
+//     name: "ahmed",
+//     title: "Software Engr"
+// };
+
+// testing(myData.name);
+
+// a2 - (2ab) + b2
+
+
+
+
+

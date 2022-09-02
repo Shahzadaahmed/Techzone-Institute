66a8d941c6e38350501697222a55f9939bf4185e
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-09-02 20:47:35 +0500

    Title case functionality updated

---
+++ Class-22/app.js
@@ -0,0 +1,97 @@
+// var day = "friday";
+
+// switch (day) {
+//     case "monday":
+//         console.log("Wroking days started")
+//         break;
+
+//     case "friday":
+//         console.log("Best day!");
+//         break;
+
+//     default:
+//         break;
+// };
+
+// Title case program...!
+/*
+function titleCase(para) {
+    // console.log(para);
+
+    var splitData = para.split(" ");
+    // console.log(splitData);
+
+    for (var i = 0; i < splitData.length; i++) {
+        // console.log(splitData[i]);
+
+        splitData[i] = splitData[i].charAt(0).toUpperCase() + splitData[i].slice(1).toLowerCase();
+        // console.log(splitData[i]);
+    };
+
+    return splitData.join(" ");
+};
+
+var paragraph = "Developer who has passion FOR web and MOBILE technologies. Expert at React Stack Development. Have good command on building Mobile applications using React Native."
+var result = titleCase(paragraph);
+console.log(result);
+*/
+
+// var userInput = document.getElementById("paragraph");
+// var showResult = document.getElementById("show-data");
+
+// function titleCase() {
+//     // console.log(userInput.value);
+//     var para = userInput.value;
+
+//     var splitData = para.split(" ");
+//     // console.log(splitData);
+
+//     for (var i = 0; i < splitData.length; i++) {
+//         // console.log(splitData[i]);
+
+//         splitData[i] = splitData[i].charAt(0).toUpperCase() + splitData[i].slice(1).toLowerCase();
+//         // console.log(splitData[i]);
+//     };
+
+//     showResult.innerHTML = splitData.join(" ");
+//     // return splitData.join(" ");
+// };
+
+// var dayName = "mondayzjkciusd";
+
+// if (dayName == "monday") {
+//     console.log("Working days started!");
+// }
+
+// else if (dayName == "friday") {
+//     console.log("Working days end!");
+// }
+
+// else {
+//     console.log('Invalid Data!');
+// };
+
+// var dayName = "fridaukvidy";
+
+
+
+// switch (dayName) {
+//     case "monday":
+//         console.log("This is monday");
+//         break;
+
+//     case "friday":
+//         console.log('This is friday!');
+//         break
+
+//     default:
+//         console.log("Data Not Found!");
+// };
+
+// var myName = "ahmed";
+// console.log(myName.charAt(2));
+
+var arr = ["ahmed", "asad", "faraz", "hammad"];
+// var updateArr = arr.slice(0, 3);
+var updateArr = arr.slice(1);
+console.log(updateArr);

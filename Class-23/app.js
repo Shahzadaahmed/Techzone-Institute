d190c77d4ca92c21de3819a7f44724027a5434e7
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-09-05 20:54:45 +0500

    Class 23 updated

---
+++ Class-23/app.js
@@ -0,0 +1,72 @@
+// var heading = document.getElementById("heading")
+// heading.innerHTML = "ahmed";
+
+// var name = "ahmed";
+// heading.innerHTML = name;
+
+
+// var keyW = this;
+// console.log(keyW);
+
+// function callname(fNa, lna) {
+//     return fNa + " " + lna;
+// };
+
+// var result = callname("muhammad", "ahmed");
+// console.log(result);
+
+
+
+// var userData = {
+//     firstName: "muhammed",
+//     lastName: "ahmed",
+//     callMyName: function () {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+
+// console.log(userData.callMyName());
+
+// var imgEl = document.getElementById("img-el");
+
+
+// function arrowUp(tag) {
+//     // console.log(tag);
+//     tag.src = "./images/tiger.jpg";
+//     tag.alt = "tiger";
+// };
+
+// function arrowDown(imgEl) {
+//     imgEl.src = "./images/lion.jpg";
+//     imgEl.alt = "lion";
+// };
+
+// var randomVal = Math.floor(Math.random() * 10) + 1;
+// console.log(randomVal);
+
+// var randomValue = 12.123456;
+// console.log(Math.floor(randomValue));
+
+// var numArr = [2, 4, 16, 80, 3];
+// var maxVal = Math.max(2, 4, 16, 80, 3);
+// var minVal = Math.min(2, 4, 16, 80, 3);
+// console.log(minVal);
+
+// var val = 
+
+// var num = 8;
+// var myName = "ahmed";
+
+// var concatination = "Hi, my name is " + myName + " and the number is " + num;
+
+// var newWay = `Hi, my name is ${myName} and the number is ${num}`;
+// console.log(newWay);
+
+// var val = 23.456;
+// var val = 23.1;
+// var ceilVal = Math.ceil(val);
+// console.log(ceilVal);
+
+var val = 81;
+var sqrtVal = Math.sqrt(val);
+console.log(sqrtVal);

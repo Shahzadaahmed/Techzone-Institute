e5e62eeb572fd02c57aa32cb9d69d66dc6538ac7
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-09-27 12:38:12 +0500

    Intro to React JS

---
+++ React JS/CLass-01 Intro To React JS/app.jsx
@@ -0,0 +1,8 @@
+let rootEl = document.getElementById("root");
+const reactRoot = ReactDOM.createRoot(rootEl);
+
+const element = <div>
+    <h1> React JS </h1>
+    <h2> Hello AHmed! </h2>
+</div>
+reactRoot.render(element);

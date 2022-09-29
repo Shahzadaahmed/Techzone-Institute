1f62853dd4f00ad6bc9352277d40ccb24e87af82
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-09-29 13:01:55 +0500

    Hello world app in React JS

---
+++ React JS/hello-world/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1> Welcome to React JS </h1>
+            <h2> Hello AHmed </h2>
+        </div>
+    );
+};
+
+export default App;

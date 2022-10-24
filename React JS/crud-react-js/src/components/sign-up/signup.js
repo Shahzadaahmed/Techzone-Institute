a02ff5d2c30457a1693f20be072892c5108cec1a
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-10-24 12:31:36 +0500

    Crud App Code Updated

--- React JS/crud-react-js/src/components/sign-up/signup.js
+++ React JS/crud-react-js/src/components/sign-up/signup.js
@@ -2,14 +2,44 @@
 
 import React, { useState, useEffect } from 'react';
 
+let dummyData = [
+    {
+        id: 1,
+        name: "ahmed",
+        email: "ahmed@gmail.com",
+        password: "ahmed123"
+    },
+
+    {
+        id: 2,
+        name: "ali",
+        email: "ali@gmail.com",
+        password: "ali123"
+    },
+
+    {
+        id: 3,
+        name: "bilal",
+        email: "bilal@gmail.com",
+        password: "bilal123"
+    },
+];
+
 const SignUpScreen = () => {
 
     // Note: Handeling states here...!
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [usersList, setUsersList] = useState([]);
 
-    // Note: COmponent mounted hook...!
+    const clearStates = () => {
+        setName("");
+        setEmail("");
+        setPassword("");
+    };
+
+    // Note: Component mounted hook...!
     useEffect(() => {
         // Note: Fetching local storage DB data...!
         let dbData = localStorage.getItem("Users");
@@ -18,31 +48,52 @@ const SignUpScreen = () => {
         if (dbData == null) {
             let emptyArr = JSON.stringify([]);
             localStorage.setItem("Users", emptyArr);
-        };
+        }
+
+        else {
+            let fetchData = localStorage.getItem("Users");
+            let actualData = JSON.parse(fetchData);
+            setUsersList(actualData);
+            // console.log("Your Data: ", actualData);
+        }
     }, []);
 
     // Note: Function to registered user...!
-    // const registeredUser = () => {
-    //     let formData = {
-    //         name,
-    //         email,
-    //         password
-    //     };
-    //     // console.log(formData);
+    const registeredUser = () => {
+        let formData = {
+            name,
+            email,
+            password
+        };
+        let duplicateFound = false;
 
-    //     // Note: Saving user in local storage db...!
-    //     if (localStorage.getItem("Users") != null) {
-    //         let prevData = localStorage.getItem("Users");
-    //         prevData = JSON.parse(prevData);
-    //         prevData.push(formData);
-    //         prevData = JSON.stringify(prevData);
-    //         localStorage.setItem("Users", prevData);
-    //     }
+        for (let i = 0; i < usersList.length; i++) {
+            // console.log(usersList[i]);
 
-    //     else {
-    //         localStorage.setItem("Users", []);
-    //     }
-    // };
+            if (usersList[i].email == formData.email) {
+                duplicateFound = true;
+                break;
+            };
+        };
+
+        if (!duplicateFound) {
+            let userListClone = usersList.slice(0);
+            userListClone.push(formData);
+            setUsersList(userListClone);
+            clearStates();
+
+            let dataInStr = JSON.stringify(userListClone);
+            localStorage.setItem("Users", dataInStr);
+        }
+        else console.log("Duplicate found!");
+    };
+
+    // 3 < 2 < 1 === false
+
+    // Note: This hook will work on every update od usersList state...!
+    useEffect(() => {
+        // console.log(usersList);
+    }, [usersList]);
 
     return (
         <>
@@ -85,7 +136,7 @@ const SignUpScreen = () => {
             <br />
 
             <button
-            // onClick={registeredUser}
+                onClick={registeredUser}
             >
                 Registered User
             </button>

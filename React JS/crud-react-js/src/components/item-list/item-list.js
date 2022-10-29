2ee40bc4d79a09613ba5c6515ab40346236b4fc6
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-10-29 19:13:18 +0500

    Crud app almost completed

--- React JS/crud-react-js/src/components/item-list/item-list.js
+++ React JS/crud-react-js/src/components/item-list/item-list.js
@@ -1,11 +1,38 @@
 // Note: ItemList component...!
 
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const ItemList = () => {
+
+    // Note: Handeling states here...!
+    const [dataList, setDataList] = useState([]);
+
+    // Note: Mounted Hook...!
+    useEffect(() => {
+        let fetchItemsData = localStorage.getItem("ElectronicItemsList")
+        let dataInJSON = JSON.parse(fetchItemsData);
+        if (dataInJSON) setDataList(dataInJSON);
+    }, []);
+
     return (
         <>
             <h1> Items List Screen! </h1>
+
+            {
+                (dataList && dataList.length > 0)
+                    ?
+                    (
+                        dataList.map((item, index) => {
+                            return (
+                                <div key={index}>
+                                    <h3> {`${index + 1} ${item.itemName}`} </h3>
+                                </div>
+                            );
+                        })
+                    )
+                    :
+                    (<h2> Data Not Found! </h2>)
+            }
         </>
     );
 };

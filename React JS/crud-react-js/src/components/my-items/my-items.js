0343198fd19ff74e4e293a167f1301331a95b581
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-10-31 13:27:41 +0500

    Filter functionality removed

---
+++ React JS/crud-react-js/src/components/my-items/my-items.js
@@ -0,0 +1,13 @@
+// Note: MyItems component...!
+
+import React from 'react';
+
+const MyItems = () => {
+    return (
+        <>
+            <h1> My Items List Screen! </h1>
+        </>
+    );
+};
+
+export default MyItems;
